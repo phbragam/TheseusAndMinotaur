c39efb8913f4123473aa60d154489f778e455867
Pedro Braga <phbm.braga@gmail.com>
2022-04-23 16:44:09 -0300

    tile based movement

--- Assets/Scripts/TheseusMovement.cs
+++ Assets/Scripts/TheseusMovement.cs
@@ -8,6 +8,8 @@ public class TheseusMovement : MonoBehaviour
 {
     private PlayerInputActions playerInputActions;
     private InputAction move;
+    private bool isMoving;
+    [SerializeField] private float timeToMoveBetweenTiles;
 
 
     private void Awake()
@@ -20,46 +22,41 @@ public class TheseusMovement : MonoBehaviour
         move = playerInputActions.Player.Move;
         move.Enable();
 
-        move.performed += MovePlayer;
+        move.performed += StartMovement;
     }
 
-    private void MovePlayer(InputAction.CallbackContext obj)
+    private void StartMovement(InputAction.CallbackContext obj)
     {
-        Debug.Log("Movement Values " + move.ReadValue<Vector2>());
+        StartCoroutine("MovePlayer");
     }
+
+
+    IEnumerator MovePlayer()
+    {
+        if (isMoving == false)
+        {
+            isMoving = true;
+            Vector3 targetPos = gameObject.transform.position + (Vector3)move.ReadValue<Vector2>();
+
+            while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+            {
+                //Debug.Log((targetPos - transform.position).sqrMagnitude);
+
+                // not doing this movement inside update or fixed update,
+                // because of this I'm not using Time.deltaTime or Time.fixedDeltaTime
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, 1 / (timeToMoveBetweenTiles * 60));
+                yield return new WaitForSeconds(1 / (timeToMoveBetweenTiles * 60));
+            }
+
+            gameObject.transform.position = targetPos;
+            isMoving = false;
+        }
+    }
+
 
     private void OnDisable()
     {
         move.Disable();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // IEnumerator TileMovement(){
-
-    // }
-
-
-
-    public void MovementTest()
-    {
-        Debug.Log("movement");
-        Debug.Log("Movement Values " + move.ReadValue<Vector2>());
-    }
-
-    private void FixedUpdate()
-    {
-        // if (move.ReadValue<Vector2>().x != 0 || move.ReadValue<Vector2>().y != 0)
-        //     Debug.Log("Movement Values " + move.ReadValue<Vector2>());
-    }
 }
